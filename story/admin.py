2cc2d28339ae6a60d778fba09a536de194619f7d
Mohammed Hakmi <mohammed.hakmiii555@gmail.com>
2022-11-14 22:38:36 +0100

    populating mock data

--- story/admin.py
+++ story/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Author)
+admin.site.register(models.Category)
+admin.site.register(models.Story)
+admin.site.register(models.Chapter)
--- story/models.py
+++ story/models.py
@@ -5,15 +5,21 @@ from django.db import models
 class Author(models.Model):
   first_name= models.CharField(max_length=255)
   last_name= models.CharField(max_length=255)
-  username= models.CharField(max_length=255)
+  username= models.CharField(max_length=255, unique=True)
   description= models.TextField(null=True, blank=True)
   email = models.EmailField()
   phone = models.CharField(max_length=255)
   created_at = models.DateField(auto_now_add=True)
 
+  def __str__(self) -> str:
+    return self.username
+
 # Category
 class Category(models.Model):
   title= models.CharField(max_length=255)
+  
+  def __str__(self) -> str:
+    return self.title
 
 # Story
 class Story(models.Model):
@@ -21,8 +27,15 @@ class Story(models.Model):
   title= models.CharField(max_length=500)
   author= models.ForeignKey(Author, on_delete=models.CASCADE)
   created_at= models.DateField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.title
 # Chapter
 class Chapter(models.Model):
   title= models.CharField(max_length=500)
   story= models.ForeignKey(Story, on_delete=models.CASCADE)
+  body= models.TextField()
   created_at= models.DateField(auto_now_add=True)
+
+  def __str__(self) -> str:
+    return self.title
